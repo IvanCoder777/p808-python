ff9cedae11cd830c6ff975258229b74aecd1544c
unknown <megalithaze@gmail.com>
2024-01-09 17:30:56 +0400

    updated 09.01.2024

--- app/order/urls.py
+++ app/order/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('basket', views.basket, name='basket'),
+    path('checkout', views.checkout, name='checkout'),
+    path('wishlist', views.wishlist, name='wishlist'),
 ]

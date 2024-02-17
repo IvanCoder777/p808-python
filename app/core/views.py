b62add74025ca6fceaf84bb50276ce6326b9a47d
unknown <megalithaze@gmail.com>
2024-02-17 17:53:08 +0400

    updated 17.02.2024

--- app/core/views.py
+++ app/core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.utils.translation import gettext as _
+from django.core.mail import send_mail
+from account.models import SubscribedUser
 from product.models import Category, Product
 
 
@@ -13,3 +17,29 @@ def index(request):
         'just_arrived_products': just_arrived_products
     }
     return render(request, 'home/index.html', context)
+
+
+def subscribe(request):
+    email = request.POST.get('subscribed_user')
+
+    if request.user.is_authenticated:
+        if request.user.email == email:
+            if not SubscribedUser.objects.filter(email=email).first():
+                SubscribedUser.objects.create(email=email)
+                messages.success(request, _('Congratulations! You have subscribed successfully!'))
+                # send_mail(
+                #     'Subscription',
+                #     'Congratulations! You have subscribed successfully!',
+                #     'notificationtodo@gmail.com',
+                #     [request.user.email]
+                # )
+                return redirect('/')
+            else:
+                messages.error(request, _('Ooops! You have subscribed already!'))
+                return redirect('/')
+        else:
+            messages.error(request, _("Ooops! This email doesn't belong to you!"))
+            return redirect('/')
+    else:
+        messages.error(request, _("Looks like you are not logged in, please log in to subscribe"))
+    return redirect('/')
